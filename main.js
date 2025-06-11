9db5faf1fb84c635aad11b5145a33dc430d8253e
simofy <juliussi@wix.com>
2025-06-11 17:53:57 +0300

    url tweaks

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import dotenv from "dotenv";
 import schedule from "node-schedule";
 
-import { handleUpluad } from "./instagram_upload.js";
+import { generateCalendarForToday } from "./calendar_worker.js";
 import {
   generateHoroscopeStoryImageCanvasPage1,
   generateHoroscopeStoryImageCanvasPage2,
 } from "./horoscope_worker.js";
+import { handleUpluad } from "./instagram_upload.js";
 
 dotenv.config();
 
@@ -17,8 +18,7 @@ const horoscopeJob = schedule.scheduleJob("0 2 * * *", () => {
 
 const calendarJob = schedule.scheduleJob("0 1 * * *", () => {
   console.log("Running scheduled calendar job at " + new Date());
-  handleUpluad("horoscope_1", generateHoroscopeStoryImageCanvasPage1);
-  handleUpluad("horoscope_2", generateHoroscopeStoryImageCanvasPage2);
+  handleUpluad("calendar", generateCalendarForToday);
 });
 
 console.log("Jobs scheduled.");
